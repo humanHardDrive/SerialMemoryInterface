0e2c5b963ed6352cabb034919492a01896d711e9
Sam Stratter <humanharddrive@gmail.com>
2018-10-11 19:57:16 -0400

    Start serial packet state machine

--- Hardware/SerialMemoryInterface/Commander.cpp
+++ Hardware/SerialMemoryInterface/Commander.cpp
@@ -1,5 +1,13 @@
 #include "Commander.h"
 
+enum
+{
+  WAITING_FOR_STX,
+  WAITING_FOR_LEN,
+  WAITING_FOR_DATA,
+  WAITING_FOR_ETX
+};
+
 void Commander_RequestMemory(ADDRESS_BUS_TYPE address, uint8_t len)
 {
   Serial.write(PACKET_STX);
@@ -25,6 +33,22 @@ void Commander_WriteMemory(ADDRESS_BUS_TYPE address, void* data, uint8_t len)
 
 void Commander_Background(uint8_t c)
 {
-  
+  static uint8_t ParseState = WAITING_FOR_STX;
+  static uint8_t
+
+  switch(ParseState)
+  {
+    case WAITING_FOR_STX:
+    break;
+
+    case WAITING_FOR_LEN:
+    break;
+
+    case WAITING_FOR_DATA:
+    break;
+
+    case WAITING_FOR_ETX:
+    break;
+  }
 }
 
