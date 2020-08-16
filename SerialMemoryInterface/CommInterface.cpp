44e607642e221fb94f8f822013302af4e161a523
Sam Stratter <humanHardDrive@gmail.com>
2020-08-15 22:45:22 -0400

    Process messages that come in over serial

--- SerialMemoryInterface/CommInterface.cpp
+++ SerialMemoryInterface/CommInterface.cpp
@@ -1,10 +1,13 @@
 #include "CommInterface.h"
 
 #include <exception>
+#include <iostream>
 
 CommInterface::CommInterface(const std::string& sDeviceName, boost::asio::io_context& ioContext) :
 	m_SerialPort(ioContext),
-	m_sDeviceName(sDeviceName)
+	m_sDeviceName(sDeviceName),
+	m_pCurrentMessageHeader(nullptr),
+	m_pCurrentMessage(nullptr)
 {
 }
 
@@ -21,13 +24,14 @@ void CommInterface::run()
 		m_SerialPort.set_option(boost::asio::serial_port::character_size(8));
 		m_SerialPort.set_option(boost::asio::serial_port::stop_bits(boost::asio::serial_port::stop_bits::one));
 
-		/*Setup the ansynchronous reads*/
+		/*Setup the asynchronous reads*/
 		boost::asio::async_read(m_SerialPort, boost::asio::buffer(m_SerialBuf), boost::asio::transfer_at_least(1), 
-			[this](const boost::system::error_code& errorCode, size_t nBytesCount)
+			[this](const boost::system::error_code& /*errorCode*/, size_t nBytesCount)
 		{
-			/*Unreferenced*/
-			errorCode;
-			nBytesCount;
+#ifdef _DEBUG
+			std::cout << "Got " << nBytesCount << " bytes" << std::endl;
+#endif
+			stateProcess(nBytesCount);
 		});
 	}
 	else
@@ -39,4 +43,48 @@ void CommInterface::run()
 
 void CommInterface::stop()
 {
+}
+
+void CommInterface::stateProcess(size_t nBytes)
+{
+	for(size_t i = 0; i < nBytes; i++)
+	{
+		//Start of a new message
+		if (!m_pCurrentMessageHeader)
+		{
+#ifdef _DEBUG
+			std::cout << "New message" << std::endl;
+#endif
+			memset(&m_CurrentMessageHeader, 0, sizeof(MessageHeader));
+			m_pCurrentMessageHeader = (uint8_t*)&m_CurrentMessageHeader;
+			m_pCurrentMessage = nullptr; //Reset the message data pointer
+		}
+		//All of the data for the header has been collected
+		else if (m_pCurrentMessageHeader && ((uint8_t*)&m_CurrentMessageHeader + sizeof(MessageHeader)) == m_pCurrentMessageHeader)
+		{
+#ifdef _DEBUG
+			if (!m_pCurrentMessage)
+				std::cout << "Processing message cmd=" << m_CurrentMessageHeader.cmd << " length=" << m_CurrentMessageHeader.len << std::endl;
+#endif
+			if (m_aProcessingFn[m_CurrentMessageHeader.cmd])
+				m_aProcessingFn[m_CurrentMessageHeader.cmd](m_SerialBuf[i]);
+
+			//Use an ETX to call the processing function at least once (for 0 length messages)
+			if (!m_pCurrentMessageHeader && m_SerialBuf[i] == SERIAL_ETX)
+			{
+				m_pCurrentMessageHeader = nullptr; //Reset the message header pointer
+#ifdef _DEBUG
+				std::cout << "Message end" << std::endl;
+#endif
+			}
+			else
+				m_CurrentMessageHeader.len--; //Decrement the data length
+		}
+		//Stuff data into the message header
+		else
+		{
+			*m_pCurrentMessageHeader = m_SerialBuf[i];
+			m_pCurrentMessageHeader++;
+		}
+	}
 }
--- SerialMemoryInterface/Headers/CommInterface.h
+++ SerialMemoryInterface/Headers/CommInterface.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <functional>
+
 #include "boost/asio.hpp"
 #include "boost/asio/serial_port.hpp"
 
@@ -14,7 +16,23 @@ public:
 	void stop();
 
 private:
+	void stateProcess(size_t nBytes);
+
+	static const uint8_t SERIAL_ETX = 0xAA;
+
+	struct MessageHeader
+	{
+		uint8_t cmd;
+		uint32_t len;
+	};
+
 	boost::asio::serial_port m_SerialPort;
 	std::string m_sDeviceName;
 	uint8_t m_SerialBuf[128];
+
+	MessageHeader m_CurrentMessageHeader;
+	uint8_t* m_pCurrentMessageHeader;
+	uint8_t* m_pCurrentMessage;
+
+	std::function<bool(uint8_t)> m_aProcessingFn[256];
 };
